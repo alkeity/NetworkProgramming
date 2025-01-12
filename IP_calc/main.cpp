00a5b796600a71b52a4f781de185c02591ea6095
alkeity <alkeity@DESKTOP-2I4CTLJ>
2025-01-13 00:07:45 +0300

    feat: mask, prefix

--- IP_calc/main.cpp
+++ IP_calc/main.cpp
@@ -1,9 +1,9 @@
 #include<Windows.h>
 #include<CommCtrl.h>
+#include<iostream>
 #include"resource.h"
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
-DWORD CreateMask(DWORD ipAddress);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
@@ -13,49 +13,76 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
+	CONST INT SIZE = 256;
 	switch (uMsg)
 	{
 	case WM_INITDIALOG:
 	{
 		HWND hUpDown = GetDlgItem(hwnd, IDC_SPIN);
 		SendMessage(hUpDown, UDM_SETRANGE, 0, MAKELPARAM(30, 1));
+		//AllocConsole();
+		//freopen("CONOUT$", "w", stdout);
 	}
 	break;
 	case WM_COMMAND:
+	{
+		DWORD ipAddress, ipMask = 0;
+		CHAR sz_buffer[SIZE];
+		HWND hIp = GetDlgItem(hwnd, IDC_IPADDRESS);
+		HWND hMask = GetDlgItem(hwnd, IDC_IPADDRESS_MASK);
+		HWND hPrefix = GetDlgItem(hwnd, IDC_EDIT_PREFIX);
 		switch (LOWORD(wParam))
 		{
-		case ID_OK:
-		{
-			HWND hIp = GetDlgItem(hwnd, IDC_IPADDRESS);
-			HWND hMask = GetDlgItem(hwnd, IDC_IPADDRESS_MASK);
-			if (SendMessage(hIp, IPM_ISBLANK, 0, 0))
+		case IDC_IPADDRESS:
+			if (HIWORD(wParam) == EN_CHANGE)
 			{
-				SendMessage(hIp, IPM_SETADDRESS, 0, MAKEIPADDRESS(0, 0, 0, 0));
-				SendMessage(hMask, IPM_SETADDRESS, 0, MAKEIPADDRESS(0, 0, 0, 0));
-			}
-			else
-			{
-				DWORD ipAddress;
-				SendMessage(hIp, IPM_GETADDRESS, 0, (LPARAM)(LPWORD)&ipAddress);
-				SendMessage(hMask, IPM_SETADDRESS, 0, CreateMask(ipAddress));
+				SendMessage(hIp, IPM_GETADDRESS, 0, (LPARAM)&ipAddress);
+				BYTE first = FIRST_IPADDRESS(ipAddress);
+				if (first < 128) ipMask = MAKEIPADDRESS(255, 0, 0, 0);
+				else if (first < 192) ipMask = MAKEIPADDRESS(255, 255, 0, 0);
+				else if (first < 256) ipMask = MAKEIPADDRESS(255, 255, 255, 0);
+				SendMessage(hMask, IPM_SETADDRESS, 0, ipMask);
+				int count = 1;
+				for (; ipMask <<= 1; count++);
+				_itoa_s(count, sz_buffer, 10);
+				SendMessage(hPrefix, WM_SETTEXT, 0, (LPARAM)sz_buffer);
 			}
 			break;
+		case IDC_IPADDRESS_MASK:
+			if (HIWORD(wParam) == EN_CHANGE)
+			{
+				SendMessage(hMask, IPM_GETADDRESS, 0, (LPARAM)&ipMask);
+				int count = 1;
+				for (; ipMask <<= 1; count++);
+				_itoa_s(count, sz_buffer, 10);
+				SendMessage(hPrefix, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+			}
+			break;
+		case IDC_EDIT_PREFIX:
+		{
+			if (HIWORD(wParam) == EN_CHANGE)
+			{
+				SendMessage(hPrefix, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+				UINT dwMask = UINT_MAX;
+				DWORD dwPrefix = atoi(sz_buffer);
+				dwMask <<= (32 - dwPrefix);
+				if (dwPrefix != 0) SendMessage(hMask, IPM_SETADDRESS, 0, dwMask);
+			}
 		}
+		case ID_OK:
+		{
+			CHAR sz_info[SIZE] = "Здесь будет информация о сети";
+			SendMessage(GetDlgItem(hwnd, IDC_STATIC_INFO_TEXT), WM_SETTEXT, 0, (LPARAM)sz_info);
+			break;
+		}
+		case ID_CANCEL: EndDialog(hwnd, 0); break;
 		default: break;
 		}
+	}
 		break;
 	case WM_CLOSE:
+		//FreeConsole();
 		EndDialog(hwnd, 0);
 	}
 	return FALSE;
-}
-
-DWORD CreateMask(DWORD ipAddress)
-{
-	DWORD ipMask = MAKEIPADDRESS(
-		FIRST_IPADDRESS(ipAddress) == 0 ? 0 : 255,
-		SECOND_IPADDRESS(ipAddress) == 0 ? 0 : 255,
-		THIRD_IPADDRESS(ipAddress) == 0 ? 0 : 255,
-		FOURTH_IPADDRESS(ipAddress) == 0 ? 0 : 255);
-	return ipMask;
 }
--- IP_calc/resource.h
+++ IP_calc/resource.h
@@ -13,6 +13,7 @@
 #define IDC_SPIN                        1008
 #define ID_OK                           1011
 #define ID_CANCEL                       1012
+#define IDC_STATIC_INFO_TEXT            1013
 
 // Next default values for new objects
 // 
@@ -20,7 +21,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        103
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1013
+#define _APS_NEXT_CONTROL_VALUE         1014
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
